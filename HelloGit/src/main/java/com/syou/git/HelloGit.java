0f7d849ab9c726d97658afc21bb68bfaccb7fa6b
SyouChew <2250297866@qq.com>
2020-08-29 21:10:05 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/syou/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.syou.git;
+
+/**
+ * @author Syou
+ * @create 2020-08-29-20:58
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
